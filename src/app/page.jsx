b5b1d0fee7109c9acabb3c81840c7eadb1b85a95
Amirreza <arazarioun83@gmail.com>
2025-04-13 17:48:30 +0330

    refactor: clean up global styles and update page metadata

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,20 +3,7 @@
 import { use, useEffect } from "react";
 
 const HomePage = () => {
+  return <>Home page</>;
+};
 
-  let input = document.getElementById("input");
-
-  useEffect(() => {
-    console.log(xx.value);
-    
-  });
-
-  
-  
-  return (<>
-    Home page
-    <input type="text" className="bg-red-400" id="input" onChange={(e) => console.log(e.target.value)}/>
-  </>);
-}
- 
 export default HomePage;
